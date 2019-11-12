559ff2ab131b56530eb747c9554377cd743718d5
MartinCohen98 <martincohen98@gmail.com>
2019-11-12 16:50:33 -0300

    Borrado de espaciado

--- src/main/java/com/codeoftheweb/salvo/com/codeoftheweb/salvo/repositories/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/com/codeoftheweb/salvo/repositories/PlayerRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-}
+public interface PlayerRepository extends JpaRepository<Player, Long> {}
